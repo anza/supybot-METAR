c9500def5a28eef469dadcc814e2667fdf98a205
Antti Louko <antti@may.fi>
2010-12-29 14:28:42 +0200

    added manual and made some minor changes

--- METAR/plugin.py
+++ METAR/plugin.py
@@ -13,28 +13,42 @@ import supybot.callbacks as callbacks
 import subprocess
 import re
 
+# this is the path to metar executable, you may need to change this
+# NOTE: the old 'metar' from eg. debian or ubuntu repos isn't suitable!
+# use this: https://github.com/anza/metar/
+metarpath = "/usr/bin/metar"
 
 class METAR(callbacks.Plugin):
 	def metar(self, irc, msg, args, reptype, station):
+		# this is the help, you want to modify this
 		"""[raw|basic|extended] <station code>
 		retrieves current weather conditions for given METAR observation station. More help and Finnish station codes: http://may.fi/~antti/metar.txt)"""
 
+		# we map wanted options with dict
 		dict = { None:'b', 'basic':'b', 'raw':'r', 'extended':'e' }
-
+		# and then get suitable option for wanted report type
 		reptype = dict[reptype]
 
-		station = station.upper()
+		# sanitize user input, allows only alphanumerical chars
 		station = re.sub('[^\w]', '', station)
+		# convert station to uppercase
+		station = station.upper()
 
-		command = 'metar' + ' -' + reptype + ' ' + station
+		# define command to execute with subprocess
+		command = metarpath + ' -' + reptype + ' ' + station
 		proc = subprocess.Popen(command,
 					shell=True,
 					stdout=subprocess.PIPE,
 					)
 		output = proc.communicate()[0]
+		# we want to strip possible newlines and hyphens
+		# from beginning and end
 		output = output.strip('\n\'')
 
+		# we get reply!
 		irc.reply(output, prefixNick=False)
+
+	# at last the wrapper..
 	metar = wrap(metar, [optional(("literal", ("raw", "basic", "extended"), 'argument parse error')), 'something'])
 
 Class = METAR
